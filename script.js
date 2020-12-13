7266c2f8ba1a2724b758d8d56b46254df0814e26
protonnote <nongnumainaka@gmail.com>
2020-12-13 15:54:51 +0700

    new edit

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ document.querySelector('#form').addEventListener('submit',function(event){
 
     event.preventDefault()
 
-    let gpa = []
-    let term = []
-    let code_ss =[]
+   // let gpa = []
+    let sem
+    //let code_ss =[]
 
     let sub  = document.getElementById('subject').value
     let code_sub  = document.getElementById('code-subject').value
@@ -25,11 +25,11 @@ document.querySelector('#form').addEventListener('submit',function(event){
 
     if(document.getElementById('term1').checked) { 
         let term1  = document.getElementById('term1').value
-        term[couter] = term1
+        sem = term1
     } 
     if(document.getElementById('term2').checked){
         let term2  = document.getElementById('term2').value 
-        term[couter] = term2
+        sem = term2
     }
     console.log(gpa)
     
@@ -51,7 +51,7 @@ document.querySelector('#form').addEventListener('submit',function(event){
         alert("โปรดกรอกชื่อวิชา !!")
         ff = false
     }
-    else if (term.length == 0 ){
+    else if (sem.length == 0 ){
         alert("โปรดเลือกภาคการศึกษา !!")
         ff = false
     }
@@ -61,13 +61,13 @@ document.querySelector('#form').addEventListener('submit',function(event){
 
     if(ff){
         
-        gpa[couter] = gpa_in
-        code_ss[couter] = code_sub
+       // gpa[couter] = gpa_in
+        //code_ss[couter] = code_sub
 
         alert("เพิ่มสำเร็จ !!")
 
         console.log(couter)
-        console.log(term)
+        console.log(sem)
         console.log(gpa)
 
         document.getElementById('subject').value = null
@@ -77,90 +77,90 @@ document.querySelector('#form').addEventListener('submit',function(event){
 
 //////////////////////////////////////////////////////////////////////////////////
 
-        if(term[couter]  === "term1"){
+        if(sem  === "term1"){
 
-            if (gpa[couter] === 'A' ){
+            if (gpa_in === 'A' ){
                 grade = grade + 4
             }
-            if(gpa[couter] === 'B+') {
+            if(gpa_in === 'B+') {
                 grade = grade + 3.5
             }
-            if(gpa[couter] === 'B') {
+            if(gpa_in === 'B') {
                 grade = grade + 3
             }
-            if(gpa[couter] === 'C+') {
+            if(gpa_in === 'C+') {
                 grade = grade + 2.5
             }
-            if(gpa[couter] === 'C') {
+            if(gpa_in === 'C') {
                 grade = grade + 2
             }
-            if(gpa[couter] === 'D+') {
+            if(gpa_in === 'D+') {
                 grade = grade + 1.5
             }
-            if(gpa[couter] === 'D') {
+            if(gpa_in === 'D') {
                 grade = grade + 1
             }
-            if (gpa[couter] === 'F') {
+            if (gpa_in === 'F') {
                 grade = grade + 0
             }
             console.log(grade)
         }
         //////////////////////////////////////////////////////////////////////////////////
-        if(term[couter]  === "term2"){
+        if(sem === "term2"){
 
-            if (gpa[couter] === 'A' ){
+            if (gpa_in === 'A' ){
                 grade_2 = grade_2 + 4
             }
-            if(gpa[couter] === 'B+') {
+            if(gpa_in === 'B+') {
                 grade_2 = grade_2 + 3.5
             }
-            if(gpa[couter] === 'B') {
+            if(gpa_in === 'B') {
                 grade_2 = grade_2 + 3
             }
-            if(gpa[couter] === 'C+') {
+            if(gpa_in === 'C+') {
                 grade_2 = grade_2 + 2.5
             }
-            if(gpa[couter] === 'C') {
+            if(gpa_in === 'C') {
                 grade_2 = grade_2 + 2
             }
-            if(gpa[couter] === 'D+') {
+            if(gpa_in === 'D+') {
                 grade_2 = grade_2 + 1.5
             }
-            if(gpa[couter] === 'D') {
+            if(gpa_in === 'D') {
                 grade_2 = grade_2 + 1
             }
-            if (gpa[couter] === 'F') {
+            if (gpa_in === 'F') {
                 grade_2 = grade_2 + 0
             }
             console.log(grade_2)
         }
 
-///////////////////////////////////////////////////////////////////////////////////
+///////////////////////// Print to table 261 /////////////////////////////////////////////////////////
 
         var check = code_sub.substring(0, 3);
         if( (check === "261") || (check === "269") ){
-            if (gpa[couter] === 'A' ){
+            if (gpa_in === 'A' ){
                 grade_261 = grade_261 + 4
             }
-            if(gpa[couter] === 'B+') {
+            if(gpa_in === 'B+') {
                 grade_261 = grade_261 + 3.5
             }
-            if(gpa[couter] === 'B') {
+            if(gpa_in === 'B') {
                 grade_261 = grade_261 + 3
             }
-            if(gpa[couter] === 'C+') {
+            if(gpa_in === 'C+') {
                 grade_261 = grade_261 + 2.5
             }
-            if(gpa[couter] === 'C') {
+            if(gpa_in === 'C') {
                 grade_261 = grade_261 + 2
             }
-            if(gpa[couter] === 'D+') {
+            if(gpa_in === 'D+') {
                 grade_261 = grade_261 + 1.5
             }
-            if(gpa[couter] === 'D') {
+            if(gpa_in === 'D') {
                 grade_261 = grade_261 + 1
             }
-            if (gpa[couter] === 'F') {
+            if (gpa_in === 'F') {
                 grade_261 = grade_261 + 0
             }
 
@@ -173,30 +173,40 @@ document.querySelector('#form').addEventListener('submit',function(event){
             couter_261++
             console.log(grade_261)
         }
-//////////////////////////////////////////////////////////////////////////////////
+///////////////////////// Print to table /////////////////////////////////////////////////////////
 
-        if(term[couter]  === "term1"){
+        if(sem  === "term1"){
             const tables = document.querySelector('#showtable')
         
             const list = document.createElement('tr')
         
             const name = document.createElement('td')
             const name_1 = document.createElement('san')
-            name_1.setAttribute("style", "color: black; padding: 20px;");
+            name_1.setAttribute("style", "color: black; padding: 20px;")
             name_1.innerHTML = sub
             name.append(name_1)
         
             const code = document.createElement('td')
-            code.setAttribute("style", "color: black; padding: 10px;");
+            code.setAttribute("style", "color: black; padding: 10px;")
             code.innerHTML = code_sub
                 
             const grade_s = document.createElement('td')
-            grade_s.setAttribute("style", "color: black; padding: 30px;");
+            grade_s.setAttribute("style", "color: black; padding: 30px;")
             grade_s.innerHTML = gpa_in
-        
+
+            const Dbut = document.createElement('td')
+            const deleteButton = document.createElement('button')
+            deleteButton.innerHTML = '\u2715';
+            deleteButton.onclick = function () {
+                tables.removeChild(list);
+            };
+            
+            Dbut.append(deleteButton)
+            
             list.append(name)
             list.append(code)
             list.append(grade_s)
+            list.append(Dbut)
         
             tables.append(list)
 
@@ -211,23 +221,23 @@ document.querySelector('#form').addEventListener('submit',function(event){
             couter++
         }
         //////////////////////////////////////////////////////////////////////////////////
-        if(term[couter]  === "term2") {
+        if(sem === "term2") {
             const tables_n = document.querySelector('#showtable_2')
         
             const list_n = document.createElement('tr')
         
             const name_n = document.createElement('td')
             const name_2 = document.createElement('san')
-            name_2.setAttribute("style", "color: black; padding: 20px;");
+            name_2.setAttribute("style", "color: black; padding: 20px;")
             name_2.innerHTML = sub
             name_n.append(name_2)
         
             const code_n = document.createElement('td') 
-            code_n.setAttribute("style", "color: black; padding: 10px;");
+            code_n.setAttribute("style", "color: black; padding: 10px;")
             code_n.innerHTML = code_sub
                 
             const grade_n = document.createElement('td')
-            grade_n.setAttribute("style", "color: black; padding: 30px;"); 
+            grade_n.setAttribute("style", "color: black; padding: 30px;")
             grade_n.innerHTML = gpa_in
         
             list_n.append(name_n)
@@ -253,3 +263,29 @@ document.querySelector('#form').addEventListener('submit',function(event){
 
 
 
+/*function CalGrade (){
+    if (gpa_in === 'A' ){
+        grade_261 += 4
+    }
+    if(gpa_in === 'B+') {
+        grade_261 += 3.5
+    }
+    if(gpa_in === 'B') {
+        grade_261 += 3
+    }
+    if(gpa_in === 'C+') {
+        grade_261 += 2.5
+    }
+    if(gpa_in === 'C') {
+        grade_261 += 2
+    }
+    if(gpa_in === 'D+') {
+        grade_261 += 1.5
+    }
+    if(gpa_in === 'D') {
+        grade_261 += 1
+    }
+    if (gpa_in === 'F') {
+        grade_261 += 0
+    }
+}*/
